16d91c6d2466fbfc707ea7b7ea7c0772e56e8406
Ankit Tiwari <ankitt255@gmail.com>
2019-08-11 01:02:35 +0530

    Use env variable for secret key

--- fyleServer/settings.py
+++ fyleServer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o%m2h)$=%ahlu)k6w!9=6$1l1df@!6w8yc518p=(-9@&(^l%nh'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
